5895fce125c7f929209b735dc25067334a31a492
Daniel Lindegren <onosendi@protonmail.com>
2018-10-29 12:26:34 -0700

    Load production config if in production environment

--- dlindegren.py
+++ dlindegren.py
@@ -4,11 +4,16 @@
 
     uWSGI entry point.
 """
+import os
+import config
 from app.app_factory import create_app
 from app.extensions import db
 
 # uWSGI entry point.
-app = create_app()
+if os.environ.get('FLASK_ENV') == 'production':
+    app = create_app(config.ProductionConfig)
+else:
+    app = create_app()
 
 
 @app.shell_context_processor
