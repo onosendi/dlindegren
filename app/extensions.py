0fa9f9fa20be3f9fc4366644919199c6039e3412
Daniel Lindegren <onosendi@protonmail.com>
2019-07-21 10:01:17 -0700

    Remove SQLAlchemy/mysqlclient - server mysql inconsistencies

--- app/extensions.py
+++ app/extensions.py
@@ -6,8 +6,3 @@
     application factory model. They are ultimately set up in
     :mod:`app.app_factory` via :func:`configure_extensions`.
 """
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-db = SQLAlchemy()
-migrate = Migrate()
