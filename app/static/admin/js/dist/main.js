3b0e64018e76c2cd5569ddf8131d3b938a93cb11
Daniel Lindegren <onosendi@protonmail.com>
2019-10-06 06:21:11 -0700

    Tried TypeScript again

--- app/static/admin/js/dist/main.js
+++ app/static/admin/js/dist/main.js
@@ -1,16 +1,4 @@
-(() => {
-  "use strict";
-
-  const sendAsync = async (url, callback) => {
-    const response = await fetch(url);
-    console.log(response);
-  };
-
-  const deleteItem = () => {
-    console.log('testing');
-  };
-
-  document.querySelector('.add-article').addEventListener('click', event => {
-    sendAsync('/admin/blog', deleteItem);
-  });
+(function () {
+    "use strict";
+    var myVar = 10;
 })();
---
+++ app/static/admin/js/src/main.ts
@@ -0,0 +1,6 @@
+(() => {
+"use strict";
+
+const myVar: number = 10;
+
+})();
