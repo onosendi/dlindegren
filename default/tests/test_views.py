02be6c69b35dd5987225ca1f4a94bddec8bf1237
Daniel Lindegren <onosendi@protonmail.com>
2020-01-08 20:15:41 -0700

    Remove 'about' page from tests

--- default/tests/test_views.py
+++ default/tests/test_views.py
@@ -5,7 +5,3 @@ class IndexViewTestCase(SimpleTestCase):
     def test_index_page_status_code(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-
-    def test_about_page_status_code(self):
-        response = self.client.get('/about/')
-        self.assertEqual(response.status_code, 200)
