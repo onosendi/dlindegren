61bd2e19b0470aa174c608e042b203393fda7f58
Daniel Lindegren <onosendi@protonmail.com>
2021-02-22 13:28:23 -0700

    upgrade.sh: remove old node modules and parcel cache

--- upgrade.sh
+++ upgrade.sh
@@ -3,6 +3,26 @@
 # Change directory to this script's location.
 cd "$(dirname "$0")"
 
+# Remove existing node modules.
+NODE_MODULES="$(pwd -P)/node_modules"
+if [ -d "$NODE_MODULES" ]; then
+  COMMAND="rm -rf ${NODE_MODULES}"
+  echo ""
+  echo "Removing old node modules..."
+  echo""
+  eval $COMMAND
+fi
+
+# Remove existing parcel cache.
+PARCEL_CACHE="$(pwd -P)/.parcel-cache"
+if [ -d "$PARCEL_CACHE" ]; then
+  COMMAND="rm -rf ${PARCEL_CACHE}"
+  echo ""
+  echo "Removing old parcel cache..."
+  echo""
+  eval $COMMAND
+fi
+
 # Install NPM packages.
 COMMAND="npm install"
 echo ""
